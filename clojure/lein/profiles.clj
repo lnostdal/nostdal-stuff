6f53dba1dbf679b156087ee51a2d5c2df49d63cb
Lars Rune Nøstdal <larsnostdal@gmail.com>
2019-09-28 16:53:57 +0300

    lein, profiles.clj: add JVM-opts needed by VisualVM ..
    .. but leave them commented out because I haven't figured out a way to
    automatically generate a new or unique port-number for each
    project (JVM-instance).

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -8,6 +8,14 @@
      (concat
       ["--illegal-access=deny"
        "-XX:-OmitStackTraceInFastThrow" ;; See http://stackoverflow.com/a/2070568/160305 ..and this is also interesting http://blogs.atlassian.com/2011/05/if_you_use_exceptions_for_path_control_dont_fill_in_the_stac/
+
+       ;; VisualVM needs this (ref: https://torsten.io/stdout/how-to-profile-clojure-code/ )
+       ;; "-Dcom.sun.management.jmxremote"
+       ;; "-Dcom.sun.management.jmxremote.port=43210"
+       ;; "-Dcom.sun.management.jmxremote.rmi.port=43210"
+       ;; "-Dcom.sun.management.jmxremote.ssl=false"
+       ;; "-Dcom.sun.management.jmxremote.authenticate=false"
+       ;; "-Dcom.sun.management.jmxremote.local.only=true" ;; Use SSH proxy as mentioned in link above!
        ]
 
       ;; OpenJ9 stuff
