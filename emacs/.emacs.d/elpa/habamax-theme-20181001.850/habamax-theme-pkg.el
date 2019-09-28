56b60ed6e5debab7c268915ab3c93083193858f0
Lars Rune Nøstdal <larsnostdal@gmail.com>
2019-09-28 15:58:44 +0300

    Add all Emacs' elpa stuff and move .emacs to dedicated emacs folder also.

---
+++ emacs/.emacs.d/elpa/habamax-theme-20181001.850/habamax-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "habamax-theme" "20181001.850" "Boring white background color that gets the job done." '((emacs "24")) :commit "6e86a1b23b6e2aaf40d4374b5673da00a28be447" :authors '(("Maxim Kim" . "habamax@gmail.com")) :maintainer '("Maxim Kim" . "habamax@gmail.com") :url "https://github.com/habamax/habamax-theme")
