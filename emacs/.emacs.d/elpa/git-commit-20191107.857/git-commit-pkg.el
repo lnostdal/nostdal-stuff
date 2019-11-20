5e690ef75724946cbf0515cbeef7b0ebb76fdd33
Lars Rune Nøstdal <larsnostdal@gmail.com>
2019-11-20 16:39:25 +0200

    Emacs: update deps

--- emacs/.emacs.d/elpa/git-commit-20191107.857/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/lnostdal/.emacs.d/elpa/git-commit-20191107.857/git-commit.el  -*- no-byte-compile: t -*-
-(define-package "git-commit" "20191107.857" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "a02dde9417bf40ed4831686be9a525bfd95290e5" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
