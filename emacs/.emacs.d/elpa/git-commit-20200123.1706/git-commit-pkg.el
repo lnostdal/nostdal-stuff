c86be5a7ab460689ef6ba86d54dcfaed20cee534
Lars Rune Nøstdal <larsnostdal@gmail.com>
2020-03-07 05:57:10 +0000

    Emacs: update deps

--- emacs/.emacs.d/elpa/git-commit-20200123.1706/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200123.1706" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "6bfe8c0957fa178eb15007055b5197f17ab2632f" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
---
+++ emacs/.emacs.d/elpa/rust-mode-20200303.932/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20200303.932" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "eb5270bc1cef48d30f33c2a312bff0638a9ff821" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
