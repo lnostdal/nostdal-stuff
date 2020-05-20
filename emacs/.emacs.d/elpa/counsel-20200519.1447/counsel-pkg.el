09eccbd7606d42e9ab9d78273d82641e17b26840
Lars Rune Nøstdal <larsnostdal@gmail.com>
2020-05-20 10:38:57 +0000

    emacs: update deps

---
+++ emacs/.emacs.d/elpa/counsel-20200519.1447/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20200519.1447" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "f1c57a1a83ecf3c253924950bedab05de2d33697" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- emacs/.emacs.d/elpa/rust-mode-20200322.1749/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20200322.1749" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "2df6cf72163db57fd0c79fefd0e79f38f29f7d93" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
