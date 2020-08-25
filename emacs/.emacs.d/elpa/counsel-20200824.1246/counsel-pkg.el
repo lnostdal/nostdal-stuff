2760d46c84939b2c92c07c5913a37521ced97a60
Lars Rune Nøstdal <larsnostdal@gmail.com>
2020-08-25 20:43:01 +0000

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/counsel-20200824.1246/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20200824.1246" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "215bb22523ac55aac8d8af376bde97364c7a91b1" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ emacs/.emacs.d/elpa/ivy-20200824.1246/ivy-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ivy" "20200824.1246" "Incremental Vertical completYon"
+  '((emacs "24.5"))
+  :commit "215bb22523ac55aac8d8af376bde97364c7a91b1" :keywords
+  '("matching")
+  :authors
+  '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
+  :maintainer
+  '("Oleh Krehel" . "ohwoeowho@gmail.com")
+  :url "https://github.com/abo-abo/swiper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
