9ddb0ae8ab97923263c3a3d8e54ee2cff6da5d31
Lars Rune Nøstdal <larsnostdal@gmail.com>
2021-09-14 19:47:32 +0000

    JDK_JAVA_OPTIONS: add -XX:+UseStringDeduplication
    seems to be saving a few 100MB ref. bec-relay, bec-metrics,
    bec-core-server with very little CPU cost.
    
    also ensure G1GC is being used.

--- jdk_java_options.sh
+++ jdk_java_options.sh
@@ -1,3 +1,3 @@
 # Symlink for /etc/profile.d/
 # NOTE: We use +IgnoreUnrecognizedVMOptions because only JVM 14+ has +ShowCodeDetailsInExceptionMessages.
-export JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} -XX:+IgnoreUnrecognizedVMOptions -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -Dclojure.core.async.go-checking=true"
+export JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} -XX:+UseStringDeduplication -XX:+IgnoreUnrecognizedVMOptions -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -Dclojure.core.async.go-checking=true"
