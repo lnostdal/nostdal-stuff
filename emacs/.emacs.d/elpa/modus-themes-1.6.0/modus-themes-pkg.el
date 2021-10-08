8e6a6c6bf2e99f67d2d646728c3d0de3ad8c4dc4
Lars Rune Nøstdal <larsnostdal@gmail.com>
2021-10-09 00:18:06 +0300

    Emacs: deps and switch to solarized-dark again (winter)

---
+++ emacs/.emacs.d/elpa/modus-themes-1.6.0/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "1.6.0" "Highly accessible themes (WCAG AAA)"
+  '((emacs "27.1"))
+  :commit "8dbfe43fe52a9420a23d29e8ca631c2b7f52d966" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/.emacs.d/elpa/parseclj-1.0.4/parseclj-pkg.el
@@ -0,0 +1,11 @@
+(define-package "parseclj" "1.0.4" "Clojure/EDN parser"
+  '((emacs "25"))
+  :commit "5809ae74cd7f5217ae29dc3bae2dba4c1c2c855a" :authors
+  '(("Arne Brasseur" . "arne@arnebrasseur.net"))
+  :maintainer
+  '("Arne Brasseur" . "arne@arnebrasseur.net")
+  :keywords
+  '("lisp" "clojure" "edn" "parser"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
