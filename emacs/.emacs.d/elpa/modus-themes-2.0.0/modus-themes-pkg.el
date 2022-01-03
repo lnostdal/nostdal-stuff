604a725b6e5a9f4552cb2bb05c6eddc4e1526e49
Lars Rune Nøstdal <larsnostdal@gmail.com>
2022-01-03 17:56:03 +0100

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/modus-themes-2.0.0/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "2.0.0" "Highly accessible themes (WCAG AAA)"
+  '((emacs "27.1"))
+  :commit "e02480f0b0a56b8575351db6504bf0d0417719ad" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
