00f31c99641402b152693a89252c721e2f1599cd
Lars Rune Nøstdal <larsnostdal@gmail.com>
2022-01-28 13:25:37 +0100

    Emacs: deps, cider

---
+++ emacs/.emacs.d/elpa/cider-20220128.716/cider-pkg.el
@@ -0,0 +1,23 @@
+(define-package "cider" "20220128.716" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "26")
+    (clojure-mode "5.12")
+    (parseedn "1.0.6")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2"))
+  :commit "a3a607365c94dd11f1981e1d884641773a8a048c" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
