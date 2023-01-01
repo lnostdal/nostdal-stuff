fb61015766f0837ab9bea2e15a72314c7f8320f3
Lars Rune Nøstdal <larsnostdal@gmail.com>
2023-01-01 13:57:59 +0000

    Emacs: deps

--- emacs/.emacs.d/elpa/modus-themes-4.0.1/modus-themes-pkg.el
+++ emacs/.emacs.d/elpa/modus-themes-4.0.1/modus-themes-pkg.el
@@ -1,6 +1,6 @@
-(define-package "modus-themes" "3.0.0" "Elegant, highly legible and customizable themes"
+(define-package "modus-themes" "4.0.1" "Elegant, highly legible and customizable themes"
   '((emacs "27.1"))
-  :commit "0bf4095918281f50f27ff3174482678c84c645d7" :authors
+  :commit "6ae40baacc070289870d8abc8d8c3abb9d961801" :authors
   '(("Protesilaos Stavrou" . "info@protesilaos.com"))
   :maintainer
   '("Modus-Themes Development" . "~protesilaos/modus-themes@lists.sr.ht")
