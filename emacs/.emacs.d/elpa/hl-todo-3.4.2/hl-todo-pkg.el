e1a8f073d4098e50ea586d6a2eba4cd7517ffba1
Lars Rune Nøstdal <larsnostdal@gmail.com>
2023-06-09 06:51:42 +0000

    Emacs: deps

--- emacs/.emacs.d/elpa/hl-todo-3.4.2/hl-todo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from hl-todo.el  -*- no-byte-compile: t -*-
-(define-package "hl-todo" "3.4.2" "highlight TODO and similar keywords" '((emacs "25")) :commit "e52285965b5ee89c18080661d4f80270143ae8dc" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("convenience") :url "https://github.com/tarsius/hl-todo")
