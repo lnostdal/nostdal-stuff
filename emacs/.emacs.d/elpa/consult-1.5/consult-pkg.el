bde763192ef4e25002987167879c5231e1aa4a5a
Lars Rune Nøstdal <larsnostdal@gmail.com>
2024-04-30 16:52:30 +0200

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/consult-1.5/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "1.5" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "d8888bb67f881a3c4855c9ce7224de18a7dc3901" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
