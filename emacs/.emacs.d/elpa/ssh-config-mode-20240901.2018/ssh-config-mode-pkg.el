598fb3c89e44a50c2202906e14828ea7b2002821
Lars Rune Nøstdal <larsnostdal@gmail.com>
2024-09-02 05:39:11 +0200

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/ssh-config-mode-20240901.2018/ssh-config-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ssh-config-mode" "20240901.2018" "Mode for fontification of ~/.ssh/config"
+  '((emacs "24.3"))
+  :commit "2d8e321c34a7535ae6dd0f6a1b0fd54e47aba612" :authors
+  '(("Harley Gorrell" . "harley@panix.com"))
+  :maintainers
+  '(("Peter Hoeg" . "peter@hoeg.com"))
+  :maintainer
+  '("Peter Hoeg" . "peter@hoeg.com")
+  :keywords
+  '("comm" "files")
+  :url "https://github.com/peterhoeg/ssh-config-mode-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
