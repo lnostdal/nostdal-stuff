e603273029977d032a4e09a37cc41add65982d9c
Lars Rune Nøstdal <larsnostdal@gmail.com>
2024-09-26 05:17:30 +0200

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/orderless-1.2/orderless-pkg.el
@@ -0,0 +1,16 @@
+(define-package "orderless" "1.2" "Completion style for matching regexps in any order"
+  '((emacs "27.1")
+    (compat "30"))
+  :commit "416c62a4a8e7199567a5df63d03cf320dc4d6ab0" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx")
+    ("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("extensions")
+  :url "https://github.com/oantolin/orderless")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
