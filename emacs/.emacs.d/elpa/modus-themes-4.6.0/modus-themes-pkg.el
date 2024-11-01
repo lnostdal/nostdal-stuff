f64880ff312540644fda2f0896db583cc5441246
Lars Rune Nøstdal <larsnostdal@gmail.com>
2024-11-01 16:17:47 +0100

    Emacs: deps

---
+++ emacs/.emacs.d/elpa/modus-themes-4.6.0/modus-themes-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "modus-themes" "4.6.0"
+  "Elegant, highly legible and customizable themes."
+  '((emacs "27.1"))
+  :url "https://github.com/protesilaos/modus-themes"
+  :commit "895e10936adac93aa8187c9cc91092dbca898677"
+  :revdesc "895e10936ada"
+  :keywords '("faces" "theme" "accessibility")
+  :authors '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainers '(("Protesilaos Stavrou" . "info@protesilaos.com")))
