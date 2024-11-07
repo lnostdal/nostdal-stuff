2394c373ee602501f02f4166783aadf0963a24d8
Lars Rune Nøstdal <larsnostdal@gmail.com>
2024-11-07 06:29:00 +0100

    Emacs: add magit conf

---
+++ emacs/.emacs.d/transient/values.el
@@ -0,0 +1,3 @@
+((magit-diff:magit-diff-mode "--diff-algorithm=histogram" "-M" "-C" "--no-ext-diff" "--stat")
+ (magit-diff:magit-revision-mode "--diff-algorithm=histogram" "-M" "-C" "--no-ext-diff" "--stat")
+ (magit-diff:magit-status-mode "--diff-algorithm=histogram" "-M" "-C" "--no-ext-diff"))
